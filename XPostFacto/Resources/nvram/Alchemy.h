4110092c39067cec321602009942d4d794ca01b5
ryan <ryan>
2002-10-07 23:17:11 +0000

    Fix the Alchemy nvram patch to match Pierre d'Herbemont's work.

--- XPostFacto/Resources/nvram/Alchemy.h
+++ XPostFacto/Resources/nvram/Alchemy.h
@@ -22,13 +22,15 @@
 # @APPLE_LICENSE_HEADER_END@
 ##
 # Version: 1.2.0  Date: 8-17-2000
+#
+# Modified by Pierre d'Herbemont to deal with certain difficulties with the 6400
+#
+##
+
 use-nvramrc? true
-load-base 600000
+load-base 100000
 diag-device 
 nvramrc hex\
-code ictc! 7E9BFBA6 l, 829F0000 l, 3BFF0004 l, 4E800020 l, c;\
-: $G FFFF0000 pvr@ and dup 80000 = swap C0000 = or ;\
-: $I $G if ictc! else drop then ;\
 : $D find-device ;\
 : $E device-end ;\
 : $L BLpatch ; : $R BRpatch ;\
@@ -40,12 +42,11 @@ code ictc! 7E9BFBA6 l, 829F0000 l, 3BFF0004 l, 4E800020 l, c;\
 dev kbd\
 get-key-map km swap move\
 $E\
-: patch " screen" " nopen" execute-device-method drop ;
+: patch " screen" " nopen" execute-device-method drop ;\
 : ck 0 do swap dup 3 >> km + c@ 1 rot 7 and << and or loop ;\
 : bootr patch 0d word count encode-string " machargs" $a\
 0 0 1 ck if 0 and else dup 1 = if 3d 0 1 else f 3d 0 2 then ck if 40 or then then\
-40 and if bye else 11 $I 1e 0 do ['] boot catch ['] init-program catch ['] go catch\
-drop 1f4 ms loop then 0 $I bye ;\
+40 and if bye else 1e 0 do ['] boot catch ['] init-program catch ['] go catch drop 1f4 ms loop then bye ;\
 : myboot boot-command eval ;\
 dev /packages/mac-parts\
 : $M -7E9408 $X 8000 alloc-mem 7F00 + 4 -7E9408 $X ;\
@@ -54,20 +55,19 @@ dev /packages/mac-parts\
 $E\
 dev ata\
 d encode-int 2 encode-int encode+ " AAPL,interrupts" $p\
+" ohare-ata" encode-string " compatible" property\
 $E\
-: $s " screen" ;\
 dev screen \
 : $NN 280 1e0 over 14 - char-width / over 14 - char-height / fb8-install ;\
-: $EE -7d78e0 $X 280 1e0 * ff filldefault-font set-font ;\
+: $EE -7d78e0 $X 280 1e0 * ff fill default-font set-font ;\
 : $RR 280 #columns char-width * - 2/ to window-left 1e0 #lines char-height * - 2/ to window-top ; \
 : $SP frame-buffer-adr encode-int " address" property \
 280 encode-int " width" property \
 1e0 encode-int " height" property \
 8 encode-int " line-bytes" property ; \
-: $WW -efff000 $X dup dup 280 1e0 * w___ do-map to frame-buffer-adr ['] $EE catch \
+: $WW -efff000 dup dup 280 1e0 * w___ do-map to frame-buffer-adr ['] $EE catch \
 ['] $NN catch \
 ['] $RR catch drop ['] $SP catch drop ; \
-: nopen $WW 0 4B00 do-unmap ; \
+: nopen $WW 0 4B000 do-unmap ; \
 $E\
-unselect-dev\
-: fix $s " $WW" $Xd ;
+unselect-dev
