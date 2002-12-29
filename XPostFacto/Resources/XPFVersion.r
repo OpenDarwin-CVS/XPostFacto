a9a362c3545d0577483b2fe13f75a8c5d3495ed0
ryan <ryan>
2002-12-29 01:17:00 +0000

    Bump version to 2.2.5b4.

--- XPostFacto/Resources/XPFVersion.r
+++ XPostFacto/Resources/XPFVersion.r
@@ -51,10 +51,10 @@ RESOURCE 'vers' (1,
 	0x02,
 	0x25,
 	beta,
-	0x3,
+	0x4,
 	verUs,
-	"2.2.5b3",
-	"2.2.5b3, © 2001, 2002 Other World Computing"
+	"2.2.5b4",
+	"2.2.5b4, © 2001, 2002 Other World Computing"
 };
 
 RESOURCE 'vers' (3,
