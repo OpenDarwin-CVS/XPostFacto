2e92aad56a831467f244b440f2a1f93b01a4551e
ryan <ryan>
2003-10-15 14:13:23 +0000

    Remove the XPFSync application, since we're not using it anymore. And move the XPFSyncPaths code into xpfbootsupportd.

--- XPFStartupItem/xpfbootsupportd.c
+++ XPFStartupItem/xpfbootsupportd.c
@@ -48,8 +48,6 @@
 #include <IOKit/IOMessage.h>
 #include <ApplicationServices/ApplicationServices.h>
 
-#include "XPFSyncPaths.h"
-
 #define XPF_SECONDS_TO_SLEEP 12
 
 // ================
@@ -95,6 +93,99 @@ void checkForChangesBetween (XPFBootFile *bf1, XPFBootFile *bf2);
 void exitWithError (OSStatus err, int code);
 void turnOffSleepIfUnsupported ();
 void setupRestartInMacOS9 ();
+
+int getPaths (char *rootDevicePath, char *bootDevicePath);
+int getMountPointForOFPath (char *mountPoint, const char *ofPath);
+
+int 
+getMountPointForOFPath (char *mountPoint, const char *ofPath)
+{ 
+	mach_port_t iokitPort;
+	IOMasterPort (MACH_PORT_NULL, &iokitPort);
+	
+	int err = 1;
+	char deviceBSDName[255];
+	strcpy (deviceBSDName, "/dev/");
+	IOServiceOFPathToBSDName (iokitPort, ofPath, deviceBSDName + 5);
+
+	mountPoint[0] = 0;
+	int numFS = getfsstat (NULL, 0, MNT_NOWAIT);
+	struct statfs *fs = (struct statfs *) malloc (numFS * sizeof (struct statfs));
+	getfsstat (fs, numFS * sizeof (struct statfs), MNT_NOWAIT);
+	int x;
+	for (x = 0; x < numFS; x++) {
+		if (!strcmp (fs[x].f_mntfromname, deviceBSDName)) {
+			strcpy (mountPoint, fs[x].f_mntonname);
+			err = 0;
+			break;
+		}
+	}		
+	free (fs);
+	
+	if (err) syslog (LOG_ERR, "Could not find mountpoint for %s", ofPath);
+	
+	return err;
+}
+
+int
+getPaths (char *rootDevicePath, char *bootDevicePath)
+{
+	char bootDeviceOFPath[256], rootDeviceOFPath[256];
+	CFMutableDictionaryRef properties;
+	io_registry_entry_t options;
+	mach_port_t iokitPort;
+	int err;
+	
+	// Get the boot-device and boot-args
+	
+	IOMasterPort (MACH_PORT_NULL, &iokitPort);
+	options = IORegistryEntryFromPath (iokitPort, "IODeviceTree:/options");
+	if (!options) return 1;
+	
+	IORegistryEntryCreateCFProperties (options, &properties, kCFAllocatorDefault, kNilOptions);
+	if (!properties) return 2;
+	
+	CFStringRef bootDevice = CFDictionaryGetValue (properties, CFSTR ("boot-device"));
+	CFStringRef bootArgs = CFDictionaryGetValue (properties, CFSTR ("boot-args"));
+	
+	CFStringGetCString (bootDevice, bootDeviceOFPath, 255, CFStringGetSystemEncoding ());
+	CFStringGetCString (bootArgs, rootDeviceOFPath, 255, CFStringGetSystemEncoding ());
+	
+	// The root-device is either found in the rd=* argument, or is equal to the boot-device
+	
+	char *pos = strstr (rootDeviceOFPath, "rd=*");
+	if (pos) {
+		pos += 4;
+		char *endpos = strstr (pos, " ");
+		if (endpos) *endpos = 0;
+		memmove (rootDeviceOFPath, pos, strlen (pos) + 1);
+	} else {
+		strcpy (rootDeviceOFPath, bootDeviceOFPath);
+	}
+	
+	// Get the mount point that corresponds to the root-device and boot-device
+	
+	err = getMountPointForOFPath (rootDevicePath, rootDeviceOFPath); if (err) return err;
+	err = getMountPointForOFPath (bootDevicePath, bootDeviceOFPath); if (err) return err;
+	
+	// See if any synchronization is required. If so, construct the path to the helper files
+	// on the boot device
+	
+	if (strcmp (rootDevicePath, bootDevicePath)) {
+		pos = rootDeviceOFPath;
+		while (*pos != 0) {
+			if (*pos == ':') *pos = ';';
+			pos++;
+		}
+		strcat (bootDevicePath, "/.XPostFacto/");
+		strcat (bootDevicePath, rootDeviceOFPath);
+	}
+	
+	CFRelease (properties);
+	IOObjectRelease (options);
+	
+	return 0;
+}
 
 void
 setupRestartInMacOS9 ()
