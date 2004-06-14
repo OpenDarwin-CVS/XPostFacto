b85517a84a52dd07e1b2c298a94edb94eaa5b1cb
ryan <ryan>
2004-06-14 15:57:06 +0000

    Don't use the -c argument to disable OWCCacheConfig. I am deleting it instead, which works more reliably.

--- Extensions/OWCCacheConfig/OWCCacheConfig.cpp
+++ Extensions/OWCCacheConfig/OWCCacheConfig.cpp
@@ -51,11 +51,9 @@ OWCCacheConfig::start (IOService *provider)
 	
 	// If we started up in single-user mode, then we refuse to start. The logic is that one of the
 	// reasons we might be in single-user mode is because we're causing trouble, and the user wants
-	// to delete us. We also refuse to start if "-c" is specified, which allows the user to turn us
-	// off without booting in single-user mode.
+	// to delete us.
 	
 	char buffer[16];
-	if (PE_parse_boot_arg ("-c", buffer)) return false;
 	if (PE_parse_boot_arg ("-s", buffer)) return false;
 	
 	// Wait a little for AppleVIA to show up, because that's what gets us an accurate CPU speed
