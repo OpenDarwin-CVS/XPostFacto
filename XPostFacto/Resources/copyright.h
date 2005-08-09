e46c70ba9f08bdff3fa29fbcf2b797256c822dab
ryan <ryan>
2005-08-09 02:13:00 +0000

    Update copyright in "About Box".

--- XPostFacto/Resources/copyright.h
+++ XPostFacto/Resources/copyright.h
@@ -1,6 +1,18 @@
-This program was written with the MacApp¨ Application Framework. ©1983-2001 Apple Computer, Inc. MacApp is a trademark of Apple Computer, Inc., registered in the United States and certain other countries. 
+XPostFacto is a partially open-source application provided by Other World Computing. There are some restrictions on the changes which you can make to it, and some conditions to be met if you want to redistribute it. In particular, you cannot remove the credits to Other World Computing. You also cannot charge for redistributing XPostFacto (or bundle XPostFacto with something that you charge for) without permission.
 
-The XPostFacto application is Copyright (c) 2001 - 2003, Other World Computing, All rights reserved.
+Parts of the source code for XPostFacto are subject to a less restrictive license. If you wish to reuse code from XPostFacto in another application, consult the specific files involved for licensing provisions. (Generally speaking, the user interface code is subject to a more restrictive license, whereas the code relating to the underlying functionality is less restricted).<p>
+
+--
+
+BootX and most of the kernel extensions which are installed by XPostFacto are subject to the <a href="http://www.opensource.apple.com/apsl/">Apple Public Source License</a>. Again, consult the specific files involved for licensing provisions.<p>
+
+--
+
+XPostFacto as a whole is subject to the following license:
+
+Copyright (c) 2001 - 2005
+Other World Computing
+All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
@@ -8,8 +20,46 @@ Redistribution and use in source and binary forms, with or without modification,
 
 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
-3. Redistributions in binary form must retain the link to Other World Computing's web site in the application's "About Box."
+3. Redistributions in binary form must retain unchanged the elements of the application which credit Other World Computing (such as the splash screen and the "about box").
+
+4. Redistributions in binary form must not require payment (or be bundled with items requiring payment). 
 
 This software is provided by Other World Computing ``as is'' and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall Ryan Rempel or Other World Computing be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 
-BootX and the kernel extensions installed by XPostFacto are subject to the Apple Public Source Licence.
+--
+
+XPostFacto was written with the MacApp Application Framework. (c) 1983-2001 Apple Computer,  Inc. MacApp is a trademark of Apple Computer, Inc., registered in the United States and certain other countries.
+
+--
+
+BootX includes the "RSA Data Security, Inc., MD5 message-digest algorithm", which is subject to the following copyright:
+
+Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All rights reserved.
+
+License to copy and use this software is granted provided that it is identified as the "RSA Data Security, Inc. MD5 Message-Digest Algorithm" in all material mentioning or referencing this software or this function.
+
+License is also granted to make and use derivative works provided that such works are identified as "derived from the RSA Data Security, Inc. MD5 Message-Digest Algorithm" in all material mentioning or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either the merchantability of this software or the suitability of this software for any particular purpose. It is provided "as is" without express or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this documentation and/or software.
+
+--
+
+BootX also includes code relating to AES encryption which is subject to the following copyright:
+
+Copyright (c) 2003, Dr Brian Gladman, Worcester, UK.   All rights reserved.
+
+LICENSE TERMS
+
+The free distribution and use of this software in both source and binary form is allowed (with or without changes) provided that:
+
+1. distributions of this source code include the above copyright notice, this list of conditions and the following disclaimer;
+
+2. distributions in binary form include the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other associated materials;
+
+3. the copyright holder's name is not used to endorse products built using this software without specific written permission.
+
+DISCLAIMER
+
+This software is provided 'as is' with no explicit or implied warranties in respect of its properties, including, but not limited to, correctness and/or fitness for purpose.
